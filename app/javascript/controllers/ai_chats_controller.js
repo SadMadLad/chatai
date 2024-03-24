613ee35db45fe9be81ba0d6f9e7b45fc096882f5
Saad Azam <zeroinside4u@gmail.com>
2024-03-24 07:59:42 +0500

    More updates

--- app/javascript/controllers/ai_chats_controller.js
+++ app/javascript/controllers/ai_chats_controller.js
@@ -4,7 +4,10 @@ export default class extends Controller {
   static targets = ['newUserMessage'];
   static values = { chatId: String }
 
-  newUserMessageTargetConnected() {
-    console.log('Ping')
+  async newUserMessageTargetConnected() {
+    if (!this.chatIdValue || this.chatIdValue === '') return;
+
+    const autocompletion = await fetch(`http://localhost:8000/chats/${this.chatIdValue}/autocomplete`, { method: 'post' })
+    console.log(autocompletion)
   }
 }
