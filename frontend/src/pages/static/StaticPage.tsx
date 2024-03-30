d7f1f4d26bec8c44653b5827d889ebde6ba1ce00
Saad Azam <zeroinside4u@gmail.com>
2024-03-30 07:49:38 +0500

    SOme pages

---
+++ frontend/src/pages/static/StaticPage.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/shared/NavBar";
+import Hero from "@/components/static/Hero";
+
+export default function StaticPage() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  )
+}
