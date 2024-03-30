48ebd6d79141c4e5ae8c7f478cc641dc1623459c
Saad Azam <zeroinside4u@gmail.com>
2024-03-30 09:35:55 +0500

    colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <StaticPage />
+      <LoginPage />
     </BrowserRouter>
   );
 }
--- frontend/src/components/static/Hero.tsx
+++ frontend/src/components/static/Hero.tsx
@@ -1,8 +1,9 @@
 export default function Hero() {
   return (
-    <section className="flex items-center justify-center py-12">
-      <div className="max-w-4xl">
-        <h1 className="text-5xl text-center font-black leading-tight">The Best Coolest Stuff That You Will Ever See</h1>
+    <section className="flex items-center justify-center py-12 px-4">
+      <div className="max-w-3xl text-center flex flex-col gap-4">
+        <h1 className="max-w-3xl text-3xl drop-shadow md:text-5xl px-4 font-black leading-none">The Best Coolest Stuff That You Will Ever See</h1>
+        <p className="text-primary-800 text-lg md:text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate totam ea rerum laboriosam ut quae vitae suscipit mollitia temporibus nobis.</p>
       </div>
     </section>
   )
