0bd10961e4a46a5c6a0d92f6444e77b8b3ef4639
Saad Azam <zeroinside4u@gmail.com>
2024-04-03 04:07:08 +0500

    Basic bst

---
+++ app/models/structures/avl_tree.rb
@@ -0,0 +1,19 @@
+module Structures
+  class AvlTree
+    attr_accessor :head
+
+    class Node
+      attr_accessor :left, :right, :key
+
+      def initialize(key)
+        @key = key
+        @left = nil
+        @right = nil
+      end
+    end
+
+    def initialize
+      @head = nil
+    end
+  end
+end
