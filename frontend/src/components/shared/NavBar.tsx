2b421cfa592329eb27015c7125a49ca4215921d8
Saad Azam <zeroinside4u@gmail.com>
2024-04-25 13:04:47 +0500

    Temp commit

--- frontend/src/components/shared/NavBar.tsx
+++ frontend/src/components/shared/NavBar.tsx
@@ -23,7 +23,7 @@ export default function NavBar() {
   }, []);
 
   return (
-    <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-white px-4 opacity-95 md:px-6">
+    <header className="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-white px-4 md:px-6">
       {isAuthed && <div>123123</div>}
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
