7afac85b8ed306b69e71337c83607df5352c63f9
Saad Azam <zeroinside4u@gmail.com>
2024-04-25 13:04:47 +0500

    Temp commit

--- frontend/src/components/static/Newsletter.tsx
+++ frontend/src/components/static/Newsletter.tsx
@@ -4,12 +4,12 @@ import { Input } from "@/components/ui/Input";
 export default function Newsletter() {
   return (
     <section className="flex items-center justify-center p-12 sm:p-20">
-      <div className="container flex items-center justify-center rounded-xl bg-black p-8">
-        <form>
+      <div className="container rounded-xl bg-black p-8">
+        <form className="flex flex-col items-center justify-center">
           <Label className="mb-2 text-xl font-bold text-white">
             Subscribe to our Newsletter
           </Label>
-          <div className="relative mt-2.5">
+          <div className="relative">
             <Input
               type="search"
               placeholder="Search products..."
