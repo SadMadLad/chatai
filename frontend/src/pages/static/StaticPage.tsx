e985ee768aa400e3a6c8f6d53305e091cca9aa60
Saad Azam <zeroinside4u@gmail.com>
2024-05-02 16:04:11 +0500

    Remove lame components

--- frontend/src/pages/static/StaticPage.tsx
+++ frontend/src/pages/static/StaticPage.tsx
@@ -1,16 +1,12 @@
 import NavBar from "@/components/shared/NavBar";
 import Footer from "@/components/static/Footer";
 import Hero from "@/components/static/Hero";
-import Services from "@/components/static/Services";
-import Newsletter from "@/components/static/Newsletter";
 
 export default function StaticPage() {
   return (
     <>
       <NavBar />
       <Hero />
-      <Services />
-      <Newsletter />
       <Footer />
     </>
   );
