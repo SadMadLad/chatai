a24d9e038cc3a3846489981835385aa3f7c3e94d
Saad Azam <zeroinside4u@gmail.com>
2024-05-03 04:07:32 +0500

    Some auth stuff

---
+++ channel/lib/channel/auth/account.ex
@@ -0,0 +1,16 @@
+defmodule Channel.Auth.Account do
+  use Ecto.Schema
+  alias Channel.Auth.AccountToken
+
+  schema "accounts" do
+    field :username, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :latest_message_at, :naive_datetime
+    field :user_id, :integer
+
+    has_many :account_tokens, AccountToken
+
+    timestamps(type: :utc_datetime, inserted_at: :created_at)
+  end
+end
--- channel/mix.exs
+++ channel/mix.exs
@@ -42,7 +42,8 @@ defmodule Channel.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:guardian, "~> 2.3"}
+      {:guardian, "~> 2.3"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
