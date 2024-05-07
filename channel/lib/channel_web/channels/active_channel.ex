cd56f81f67142fa0e41602ac1b9ddf784d73a976
Saad Azam <zeroinside4u@gmail.com>
2024-05-08 03:53:04 +0500

    Fix some stuff

--- channel/lib/channel_web/channels/active_channel.ex
+++ channel/lib/channel_web/channels/active_channel.ex
@@ -2,15 +2,19 @@ defmodule ChannelWeb.ActiveChannel do
   use ChannelWeb, :channel
   alias ChannelWeb.Presence
 
-  def join("active:lobby", %{"name" => name}, socket) do
+  def join("active:lobby", %{"token" => token}, socket) do
     send(self(), :after_join)
-    {:ok, assign(socket, :name, name)}
+    {:ok, assign(socket, :token, token)}
   end
 
   def handle_info(:after_join, socket) do
+    first_name = socket.assigns.current_account.first_name
+    last_name = socket.assigns.current_account.last_name
+
     {:ok, _} =
-      Presence.track(socket, socket.assigns.name, %{
-        online_at: inspect(System.system_time(:second))
+      Presence.track(socket, socket.assigns.token, %{
+        online_at: inspect(System.system_time(:second)),
+        name: first_name <> " " <> last_name
       })
 
     push(socket, "presence_state", Presence.list(socket))
