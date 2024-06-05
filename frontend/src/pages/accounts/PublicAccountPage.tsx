e8a1beea26258cd315c05e900bb46cb57469c679
Saad Azam <zeroinside4u@gmail.com>
2024-06-05 22:39:36 +0500

    Temp commit

--- frontend/src/pages/accounts/PublicAccountPage.tsx
+++ frontend/src/pages/accounts/PublicAccountPage.tsx
@@ -1,74 +1,47 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-import useAuthStore from "@/storage/useAuthStore";
+import useFetch from "@/hooks/useFetch";
 
 import ApplicationLayout from "@/layouts/ApplicationLayout";
-import AccountsHeaderSection from "@/components/accounts/AccountsHeaderSection";
 import AccountChatsSection from "@/components/accounts/AccountChatsSection";
+import AccountsHeaderSection from "@/components/accounts/AccountsHeaderSection";
 import ChatPageSkeleton from "@/components/skeletons/ChatPageSkeleton";
 
-import { client } from "@/services/clients";
 import { RailsRoutes } from "@/services/routes";
-import { AccountData } from "@/types/data/UserTypes";
-import { Chat, Message } from "@/types/data/ChatTypes";
 
 export default function PublicAccountPage() {
   const { identifier } = useParams();
-  const { authToken } = useAuthStore();
 
-  const [accountData, setAccountData] = useState<AccountData | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [chats, setChats] = useState<Chat[]>([]);
+  const { method, url } = RailsRoutes.accounts;
+  const [fetchedData, isLoading, isSuccess] = useFetch(
+    `${url}/${identifier}/public`,
+    method,
+  );
 
-  useEffect(() => {
-    const abortController = new AbortController();
-    const signal = abortController.signal;
-
-    fetchPublicAccountDetails(signal);
-
-    return () => {
-      abortController.abort();
-    };
-  }, [identifier]);
-
-  async function fetchPublicAccountDetails(signal: AbortSignal) {
-    setIsLoading(true);
-
-    const { method, url } = RailsRoutes.accounts;
-    const response = await fetch(
-      client(`${url}/${identifier}/public`, method, { authToken: authToken }),
-      { signal },
+  if (isLoading)
+    return (
+      <ApplicationLayout>
+        <ChatPageSkeleton />
+      </ApplicationLayout>
     );
 
-    if (response.ok) {
-      const {
-        account,
-        messages,
-        chats,
-      }: { account: AccountData; messages: Message[]; chats: Chat[] } =
-        await response.json();
+  if (isSuccess && fetchedData) {
+    const { account, chats, messages }: {
+      account: AccountData;
+      chats: Chat[];
+      messages: Message[];
+    } = fetchedData;
 
-      setAccountData(account);
-      setMessages(messages);
-      setChats(chats);
-    }
-
-    setIsLoading(false);
-  }
-
-  return (
+    return (
+      <ApplicationLayout>
+        <section className="container flex flex-col gap-8 py-12 md:flex-row">
+          <AccountsHeaderSection accountData={account} />
+          <AccountChatsSection chats={chats} messages={messages} />
+        </section>
+      </ApplicationLayout>
+    );
+  } else {
     <ApplicationLayout>
-      <>
-        {isLoading && <ChatPageSkeleton />}
-        {!isLoading && (
-          <section className="container flex flex-col gap-8 py-12 md:flex-row">
-            <AccountsHeaderSection accountData={accountData} />
-            <AccountChatsSection chats={chats} messages={messages} />
-          </section>
-        )}
-      </>
-    </ApplicationLayout>
-  );
+      Something Failed. Please try again later.
+    </ApplicationLayout>;
+  }
 }
