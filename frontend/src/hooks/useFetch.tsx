d2a89fa539e51bfb9d9e524dbb977c283c0d486d
Saad Azam <zeroinside4u@gmail.com>
2024-06-05 22:39:36 +0500

    Temp commit

---
+++ frontend/src/hooks/useFetch.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+import { client } from "@/services/clients";
+import { Method } from "@/types/UtilTypes";
+import useAuthStore from "@/storage/useAuthStore";
+
+export default function useFetch(url: string, method: Method) {
+  const [fetchedData, setFetchedData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean | null>(true);
+  const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+
+  const { authToken } = useAuthStore();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    fetch(client(url, method, { authToken: authToken }), { signal })
+      .then((response) => response.json())
+      .then((data) => {
+        setFetchedData(data);
+        setIsSuccess(true);
+      })
+      .catch(() => setIsSuccess(false) )
+      .finally(() => setIsLoading(false));
+
+    return () => abortController.abort();
+  }, [url]);
+
+  return [fetchedData, isLoading, isSuccess];
+}
