15f06a37421eb6045a64d66210e62b8840022aac
Saad Azam <zeroinside4u@gmail.com>
2024-06-06 22:47:00 +0500

    Temp commit

--- db/migrate/20240531120432_create_comments.rb
+++ db/migrate/20240531120432_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.1]
     create_table :comments do |t|
       t.belongs_to :account, null: false, foreign_key: true
       t.references :commentable, polymorphic: true, null: false
-      
+
       t.text :body
 
       t.timestamps
